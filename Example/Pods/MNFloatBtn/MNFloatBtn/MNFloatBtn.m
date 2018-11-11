d270ccf1653d7d1e278530687fd0486ad7ca1039
miniLV <liangyuhangapple@gmail.com>
2018-11-11 23:40:12 +0800

    优化demo

--- Example/Pods/MNFloatBtn/MNFloatBtn/MNFloatBtn.m
+++ Example/Pods/MNFloatBtn/MNFloatBtn/MNFloatBtn.m
@@ -47,7 +47,7 @@ static CGFloat floatBtnH = 49;
 + (void)showDebugMode{
     
 #ifdef DEBUG
-[self show];
+    [self show];
 #else
 #endif
 }
@@ -85,27 +85,15 @@ static CGFloat floatBtnH = 49;
     
     NSString *title = [NSString stringWithFormat:@"Ver:%@ 测试\nBuild:%@",versionStr,buildStr];
     
-    NSBundle *bundle = [NSBundle bundleForClass:[MNFloatBtn class]];
+    UIImage *image = [self p_loadResourceImage];
     
-    NSLog(@"bundle = %@",bundle);
-    
-    NSURL *url = [bundle URLForResource:@"Resources" withExtension:@"bundle"];
-    NSBundle *imageBundle = [NSBundle bundleWithURL:url];
-    
-    NSLog(@"url = %@ , imageBundle = %@",url, imageBundle);
-    
-    NSString *path = [imageBundle pathForResource:@"mn_placeholder" ofType:@"png"];
-    
-    UIImage *image = [UIImage imageWithContentsOfFile:path];
-    
-    NSLog(@"path = %@, image = %@",path,image);
     return [self initWithType:type
                         frame:frame
                         title:title
                    titleColor:[UIColor whiteColor]
                     titleFont:[UIFont systemFontOfSize:11]
               backgroundColor:nil
-              backgroundImage:[UIImage imageNamed:@"mn_placeholder"]];
+              backgroundImage:image];
 }
 
 - (instancetype)initWithType:(MNAssistiveTouchType)type
@@ -145,7 +133,7 @@ static CGFloat floatBtnH = 49;
 - (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event{
     
     [super touchesBegan:touches withEvent:event];
-
+    
     //按钮刚按下的时候，获取此时的起始坐标
     UITouch *touch = [touches anyObject];
     _touchPoint = [touch locationInView:self];
@@ -155,7 +143,7 @@ static CGFloat floatBtnH = 49;
 }
 
 - (void)touchesMoved:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
-
+    
     UITouch *touch = [touches anyObject];
     CGPoint currentPosition = [touch locationInView:self];
     
@@ -205,25 +193,25 @@ static CGFloat floatBtnH = 49;
 }
 
 - (void)touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event{
-
+    
     CGFloat btnWidth = self.frame.size.width;
     CGFloat btnHeight = self.frame.size.height;
     CGFloat btnY = self.frame.origin.y;
     CGFloat btnX = self.frame.origin.x;
     
     CGFloat minDistance = 2;
-
+    
     //结束move的时候，计算移动的距离是>最低要求，如果没有，就调用按钮点击事件
     BOOL isOverX = fabs(btnX - _touchBtnX) > minDistance;
     BOOL isOverY = fabs(btnY - _touchBtnY) > minDistance;
-
+    
     if (isOverX || isOverY) {
         //超过移动范围就不响应点击 - 只做移动操作
         [self touchesCancelled:touches withEvent:event];
     }else{
         [super touchesEnded:touches withEvent:event];
     }
-
+    
     //按钮靠近右侧
     switch (_type) {
             
@@ -265,7 +253,20 @@ static CGFloat floatBtnH = 49;
     }
 }
 
-
+#pragma mark - loadResourceImage
+- (UIImage *)p_loadResourceImage{
+    
+    NSBundle *bundle = [NSBundle bundleForClass:[MNFloatBtn class]];
+    
+    NSURL *url = [bundle URLForResource:@"MNFloatBtn" withExtension:@"bundle"];
+    NSBundle *imageBundle = [NSBundle bundleWithURL:url];
+    
+    NSString *path = [imageBundle pathForResource:@"mn_placeholder@3x" ofType:@"png"];
+    
+    UIImage *image = [UIImage imageWithContentsOfFile:path];
+    
+    return image;
+}
 
 
 @end
